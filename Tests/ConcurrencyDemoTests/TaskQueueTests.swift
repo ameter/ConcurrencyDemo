13412acaf40eb695b84d8c6e86149f7d51e2d6ad
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-10-05 15:08:51 -0600

    add additional TaskQueue test

--- Tests/ConcurrencyDemoTests/TaskQueueTests.swift
+++ Tests/ConcurrencyDemoTests/TaskQueueTests.swift
@@ -24,6 +24,63 @@ class TaskQueueTests: XCTestCase {
         print("done")
     }
     
+    func testMultipleCalls() {
+        let finished1 = expectation(description: "finished 1")
+        let finished2 = expectation(description: "finished 2")
+        let finished3 = expectation(description: "finished 3")
+        let finished4 = expectation(description: "finished 4")
+        
+        Task {
+            print("starting 1")
+            
+            try await taskQueue.sync {
+                try? await Task.sleep(nanoseconds: 7 * NSEC_PER_SEC)
+                await self.call(1)
+            }
+            
+            print("done 1")
+            finished1.fulfill()
+        }
+        
+        Task {
+            print("starting 2")
+            
+            try await taskQueue.sync {
+                try? await Task.sleep(nanoseconds: 5 * NSEC_PER_SEC)
+                await self.call(2)
+            }
+            
+            print("done 2")
+            finished2.fulfill()
+        }
+        
+        Task {
+            print("starting 3")
+            
+            try await taskQueue.sync {
+                try? await Task.sleep(nanoseconds: 3 * NSEC_PER_SEC)
+                await self.call(3)
+            }
+            
+            print("done 3")
+            finished3.fulfill()
+        }
+        
+        Task {
+            print("starting 4")
+            
+            try await taskQueue.sync {
+                try? await Task.sleep(nanoseconds: 1 * NSEC_PER_SEC)
+                await self.call(4)
+            }
+            
+            print("done 4")
+            finished4.fulfill()
+        }
+        
+        waitForExpectations(timeout: 30)
+    }
+    
     func testAsync() throws {
         taskQueue.async {
             await self.call()
@@ -61,6 +118,10 @@ class TaskQueueTests: XCTestCase {
     private func throwingCall() async throws {
         print("Function: \(#function)")
         throw taskError.boom
+    }
+    
+    private func call(_ number: Int) async {
+        print("Function: \(#function) \(number)")
     }
 }
 
